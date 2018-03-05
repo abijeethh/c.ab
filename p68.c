a8cda0081088d8972ab4d8cf3c42ad10aaba9f74
abijeethh <35689017+abijeethh@users.noreply.github.com>
2018-03-05 13:15:21 +0530

    Create p68.c

---
+++ p68.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i,count=0;
+    printf("Enter the number");
+    scanf("%d",&n);
+        if(n%7==0)
+        {
+           printf("Yes");
+        }
+    
+    else
+    {
+        printf("No");
+    }
+    return 0;
+}
