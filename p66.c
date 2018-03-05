f013171fd7c0eec2d5a413aa45c6b86bc2269b45
abijeethh <35689017+abijeethh@users.noreply.github.com>
2018-03-05 13:10:32 +0530

    Create p66.c

---
+++ p66.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+def fact(v) :
+    if v > 1:
+        for i in range(2,v):
+            if (v % i) == 0:
+                return "no"
+        return "yes"
+    return "no"
+v = int(input(
+   "Enter value"))
+print(fact(v))
