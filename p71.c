e7df79a391efed9b59b45e2131726d2cb90987ec
abijeethh <35689017+abijeethh@users.noreply.github.com>
2018-03-05 13:42:24 +0530

    Create   p71.c

---
+++ p71.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char s[20];
+    int i,length;
+    printf("\n Enter a number:");
+    gets(s);
+    length=strlen(s);
+    for(i=0;i<=length;i++)
+    if(s{i]!=s(length-i-1))
+    {
+        printf("\n string is palindrome")
+    }
+    else
+    {
+        printf("\n String is not palindrome")
+    }
+    return 0;
+}
