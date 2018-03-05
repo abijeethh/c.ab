a38f16ca2769444e82f839a40a80723184148fb1
abijeethh <35689017+abijeethh@users.noreply.github.com>
2018-03-05 13:19:52 +0530

    Create p70.c

---
+++ p70.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+def nextPowerOf2(n):
+	count = 0
+
+	if n == 0:
+		return 1
+
+	if not (n & (n-1)):
+		return n << 1
+
+	while n != 0:
+		n = n >> 1
+		count += 1
+
+	return 1 << count
+
+n = int(raw_input())
+print(nextPowerOf2(n))
