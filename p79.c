754f38b5b1d6c361134c7f50857b655864eb90cb
abijeethh <35689017+abijeethh@users.noreply.github.com>
2018-03-07 14:40:52 +0530

    Create p79.c

---
+++ p79.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+ int m,n,pro;
+
+ scanf("%d %d",&m,&n);
+    pro=m*n;
+    if(m==n)
+    printf("yes");
+    else
+    printf("no");
+ 
+  return 0;
+}
