5adc7c69aebd2016440082306a56e8f8a88ad2be
abijeethh <35689017+abijeethh@users.noreply.github.com>
2018-03-07 14:01:07 +0530

    Create p74.c

---
+++ p74.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+float n,t;
+int temp,N;
+printf("enter the  floating point number");
+scanf("%f",&n);
+temp=n;
+t=temp+0.5;
+if(n>t)
+{
+N=temp+1;
+printf("rounded value=%d",N);
+}
+else
+{
+printf("rounded value=%d",temp);
+}
+return 0;
+}
