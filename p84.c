ce196f9f26abab39557aaf096c4f6676da1b754e
abijeethh <35689017+abijeethh@users.noreply.github.com>
2018-03-09 16:06:35 +0530

    Create p84.c

---
+++ p84.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   char ch;
+   printf("Enter the character:");
+   scanf("%c",&ch);
+   printf("The ascii value of the given character %c is : %d",ch,ch);
+   return 0;
+}
