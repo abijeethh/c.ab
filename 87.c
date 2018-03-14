ce3968067ff62a5d52325eaa0a9e7114e6490ea5
abijeethh <35689017+abijeethh@users.noreply.github.com>
2018-03-14 16:13:28 +0530

    Create 87.c

---
+++ 87.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+
+#include<conio.h>
+
+void main()
+
+
+
+
+
+{
+int n1,n2,gcd,i;
+
+scanf("%d %d",&n1,&n2);
+
+for(i=1;i<=n1&&i<=n2;i++)
+
+{
+
+i f(n1%i==0 && n2%i==0)     
+
+
+
+
+    gcd=i;
+	  
+    }
+	}
+  
+	
+  printf("gcd of %d and %d is %d",n1,n2,gcd);
+	
+  getch();
+}
+
+
+
+
